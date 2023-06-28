133f639707bf348e22838b36e9fd5a7b185c53c9
RMSSSYNF <synf30@gmail.com>
2023-06-28 21:57:25 +0200

    Font presque fini

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function chan()
     cercle.classList.add("clic-bnt-choix-play");
     triangle.classList.add("clic-triangle");
 }
+
+const cliquer = document.querySelector(".btn");
+
+cliquer.addEventListener("click", ouvre);
+const Sup = document.querySelector(".contener-head");
+const Inf = document.querySelector(".contener-footer");
+
+function ouvre(){
+    Sup.classList.add("btn-ouverture-sup");
+    Inf.classList.add("btn-ouverture-inf");
+    setTimeout(function(){Inf.style.display='none'},600);
+    setTimeout(function(){Sup.style.display='none'},600);
+    setTimeout(function(){cliquer.style.display='none'},600);
+}
+// function attente(a,b,c){
+//     a.style.display='none';
+//     b.style.display='none';
+//     c.style.display='none';
+// }
